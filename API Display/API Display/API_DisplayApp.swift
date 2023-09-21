576942f0346815fc572f69600c13360ef629ced1
Jygo547 <jygo3715@colorado.edu>
2023-09-21 12:56:23 -0600

    Initial Commit

---
+++ API Display/API Display/API_DisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_DisplayApp.swift
+//  API Display
+//
+//  Created by ATLAS Checkout 6 Guest on 9/21/23.
+//
+
+import SwiftUI
+
+@main
+struct API_DisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
